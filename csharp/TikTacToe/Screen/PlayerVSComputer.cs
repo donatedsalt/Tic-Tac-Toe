14a2af2a00b7b9b18075482ead62f4b3466abcb9
donatedsalt <mmohsinkhan3685@gmail.com>
2026-04-25 23:29:27 +0500

    feat(C#): add a stupid bot for Player vs Computer mode
    will add a bot dificulty selector later
    with 2 difficulty options:
    1. stupid bot
    2. master bot

--- csharp/TikTacToe/Screen/PlayerVSComputer.cs
+++ csharp/TikTacToe/Screen/PlayerVSComputer.cs
@@ -8,10 +8,74 @@ public static partial class Screens
 {
     public static bool PlayerVSComputer()
     {
-        Console.Clear();
-        Console.WriteLine("Not implemented yet!");
-        Console.WriteLine("Press any key to return to the start screen.");
-        Console.ReadKey();
-        return true;
+        Grid.Reset();
+
+        Player currentPlayer = Player.Player1;
+        Player? winner = null;
+
+        while (winner == null)
+        {
+            BoardScreen(GameMode.PlayerVSComputer, currentPlayer);
+
+            int position = 0;
+            if (currentPlayer == Player.Computer)
+            {
+                position = GetComputerMove();
+            }
+            else
+            {
+                position = int.Parse(Console.ReadKey().KeyChar.ToString());
+            }
+
+            int[]? coords = Grid.GetCellCoords(position);
+            if (coords == null) continue;
+
+            Player cell = Grid.GetCellPlayer(coords[0], coords[1]);
+
+            if (cell == Player.Invalid)
+            {
+                Grid.Update(coords[0], coords[1], currentPlayer == Player.Player1 ? 'X' : 'O');
+            }
+            else
+            {
+                continue;
+            }
+
+            winner = Grid.GetWinner();
+
+            currentPlayer = currentPlayer == Player.Player1 ? Player.Computer : Player.Player1;
+        }
+
+        BoardScreen(GameMode.PlayerVSComputer, winner, true);
+
+        Console.WriteLine("Press 1 to go to the main menu.");
+        Console.WriteLine("Press any other key to exit.");
+
+        if (Console.ReadKey().KeyChar == '1')
+        {
+            Console.Clear();
+            return true;
+        }
+        else
+        {
+            Console.Clear();
+            return false;
+        }
+    }
+
+    private static int GetComputerMove()
+    {
+        int position = 0;
+        while (true)
+        {
+            position = new Random().Next(1, 10);
+
+            int[]? coords = Grid.GetCellCoords(position);
+            if (coords == null) continue;
+
+            Player cell = Grid.GetCellPlayer(coords[0], coords[1]);
+            if (cell == Player.Invalid) break;
+        }
+        return position;
     }
 }
